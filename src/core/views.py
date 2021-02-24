ad9f0b2028970f2e1d36863c3dc316fcb32e82bc
David Botros <a01052944@unet.univie.ac.at>
2021-02-25 00:17:42 +0100

    function based view & added them to the urls

--- src/core/views.py
+++ src/core/views.py
@@ -1,2 +1,10 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
+# function based view to return the required data
+def test_view(request):
+    data = {
+        'name': 'John',
+        'age' : 23
+    }
+    return JsonResponse(data)
