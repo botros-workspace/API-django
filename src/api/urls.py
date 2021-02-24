32970e5225803c2cd0768f421c6bd9148d86acf0
David Botros <a01052944@unet.univie.ac.at>
2021-02-25 00:31:06 +0100

    rest_framework added & a class based view

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
-from core.views import test_view
+from django.urls import path, include 
+from core.views import Test_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', test_view, name = "test")
+    path('api-auth/', include('rest_framework.urls')),
+    path('', Test_view.as_view(), name = "test")
 ]
--- src/core/views.py
+++ src/core/views.py
@@ -1,10 +1,24 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 
-# function based view to return the required data
-def test_view(request):
-    data = {
+#third party imports
+from rest_framework.response import Response 
+from rest_framework.views import APIView
+
+class Test_view(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {
         'name': 'John',
         'age' : 23
-    }
-    return JsonResponse(data)
+        }
+        return Response(data)
+
+# # function based view to return the required data
+# def test_view(request):
+#     data = {
+#         'name': 'John',
+#         'age' : 23
+#     }
+#     return JsonResponse(data)
+#  # if we want to return another type of data like a list we add safe parameter in the return 
+#  # arrgument and set it to False -----> return JsonResponse(data,safe= Flase)
