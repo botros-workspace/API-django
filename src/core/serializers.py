7fa30dcc1d434a0e043cd503fd4da91216c3a755
David Botros <a01052944@unet.univie.ac.at>
2021-02-25 02:02:40 +0100

    serlializer added and get and post method defined

---
+++ src/core/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Post
+
+# this is a transformation between our Model in the data base into a json  
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields =(
+            'title',
+            'description'
+            )
+            
--- src/core/views.py
+++ src/core/views.py
@@ -1,17 +1,25 @@
 from django.http import JsonResponse
 from django.shortcuts import render
-
+from .serializers import PostSerializer 
+from .models import Post 
 #third party imports
 from rest_framework.response import Response 
 from rest_framework.views import APIView
 
 class Test_view(APIView):
+    # in the get method we need a query set and pass it to the serializer and in the arguments
+    # in case of getting more than one instance we need to set set many = True
     def get(self, request, *args, **kwargs):
-        data = {
-        'name': 'John',
-        'age' : 23
-        }
-        return Response(data)
+        qs = Post.objects.all()
+        serializer = PostSerializer(qs, many = True)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data) 
+        return Response(serializer.errors)
 
 # # function based view to return the required data
 # def test_view(request):
